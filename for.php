d4d202db6973648cea8e9b2c128c8d46124b9f24
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-13 11:06:11 -0500

    if statement added

--- for.php
+++ for.php
@@ -11,43 +11,23 @@ $two = (int)fgets(STDIN);
 echo ("Please enter an incrementer! ");
 $inc = (int)fgets(STDIN);
 
+if ($inc == "\n") {
+	$inc = 1;
+}
+
 $index = $min;
 
-for ($index; $index < $two; $index += $inc) {
+for ($index; $index <= $two; $index += $inc) {
 	echo ("$index\n");
-}
+} 
+
 
 //Refactor to allow user to choose increment. (count by 1, 2, 10, ...)
 
 echo "\$index has a value of {$index}\n";
 
 
-/*echo "While:\n";
-While ($min <= $b) {
-	echo " numbers from start to finish is {$min}\n";
-}
-
-echo "For:\n";
-
-$b = second
-
-echo "pick two number between 1 and 100\n";
-for ($a = 1;$a < 100; $a += 2) {
-	echo "$a\n";
-}
-
-echo "\$argv";
-print_r($argv);
-*/
+//Make sure you are only allowing users to pass in numbers. Give an error message is both passed arguments are not numeric. See php.net/is_numeric.
 
 
 
-
-
-/*
-
-
-Default increment to 1 if no input.
-
-Make sure you are only allowing users to pass in numbers. Give an error message is both passed arguments are not numeric. See php.net/is_numeric.
-*/
