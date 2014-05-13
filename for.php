845de16c642f98ef8e38bdde65dfdc5ed650f91e
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-13 10:53:33 -0500

    refavtor

--- for.php
+++ for.php
@@ -1,6 +1,3 @@
-//Prompt user for a starting number and ending number, then display all the numbers from the starting to ending using a for loop.
-
-
 <?php
 
 echo ("Please enter a start number ");
@@ -11,12 +8,18 @@ echo ("Please enter a second number ");
 
 $two = (int)fgets(STDIN);
 
+echo ("Please enter an incrementer! ");
+$inc = (int)fgets(STDIN);
+
 $index = $min;
 
-for ($index; $index <= $two; $index++) {
+for ($index; $index < $two; $index += $inc) {
 	echo ("$index\n");
 }
 
+//Refactor to allow user to choose increment. (count by 1, 2, 10, ...)
+
+echo "\$index has a value of {$index}\n";
 
 
 /*echo "While:\n";
@@ -41,7 +44,7 @@ print_r($argv);
 
 
 
-/*Refactor to allow user to choose increment. (count by 1, 2, 10, ...)
+/*
 
 
 Default increment to 1 if no input.
