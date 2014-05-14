cd793a95419cf86731f751a1a0ca5d87ab852e0d
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-14 11:14:10 -0500

    break loop

--- break_continue.php
+++ break_continue.php
@@ -2,9 +2,9 @@
 
 // Loop 
 for ($i = 1; $i <= 100; $i++) {
-        if ($i % 2 == 0) {
-            echo "{$i}\n";
-            continue;  
+    echo "{$i}\n";
+	if ($i == 10) {
+   		 break;  
     }
 }
 echo "done!\n";
