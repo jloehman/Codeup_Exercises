b4b743a4857e3abd47f878fc17dfafc32dab903a
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-15 09:46:48 -0500

    tetsing functions

---
+++ arithmetic.php
@@ -0,0 +1,32 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+add(1,2);
+
+function subtract($a, $b) {
+    // Add code here
+    echo $a - $b . PHP_EOL;
+}
+
+subtract(1,2);
+function multiply($a, $b) {
+    // Add code here
+    echo $a * $b . PHP_EOL;
+}
+multiply(1,2);
+function divide($a, $b) {
+    // Add code here
+    echo $a / $b . PHP_EOL;
+}
+divide(1,2);
+
+
+//Add a function modulus that finds the modulus of 2 numbers.
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+modulus(1,2);
+?>
+
