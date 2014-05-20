034238b64acf068295a4096947a427d2f9ec7207
JLo <jkloehman@Jasons-MacBook-Air.local>
2014-05-20 15:04:39 -0500

    function added step 2

--- list_wih_commas.php
+++ list_wih_commas.php
@@ -1,7 +1,31 @@
 <?php
 
+// Converts array into list n1, n2, ..., and n3
+function humanized_list($string, $do_sort = false) 
+{
+//assign variable
+		$array = explode(', ', $string);
+		if($do_sort){
+		sort($array);
+}
+$last_item = array_pop($array);
 
+$array[] = "and $last_item";
 
+$string = implode(', ', $array);
+
+return $string;
+  // Your solution goes here!
+}
+
+// List of famous peeps
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark ' . PHP_EOL;
+
+// Humanize that list
+$famous_fake_physicists = humanized_list($physicists_string);
+
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
 // string
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 // array
